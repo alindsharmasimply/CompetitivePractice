f44d6975b71321b6355ca6c262888ffbc3b700c7
Alind Sharma <alindsharmasimply@gmail.com>
2017-10-07 18:01:01 +0530

    Parent, right child, left child created

--- sixtySecond.cpp
+++ sixtySecond.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 struct heap {
-    int array*;
+    int *array;
     int count;
     int capacity;
     int heap_type;
 };
+heap* create(int cap, int type)
+{
+    heap* temp = new heap();
+    temp->heap_type = type;
+    temp->count = 0;
+    temp->capacity = cap;
+    temp->array = new int[cap];
+    return temp;
+}
+int parent(int i)
+{
+    return (i-1)/2;
+}
+int left(int i)
+{
+    return (2*i) + 1;
+}
+int right(int i)
+{
+    return (2*i) + 2;
+}
