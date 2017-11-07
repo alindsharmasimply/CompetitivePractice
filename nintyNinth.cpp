59103bb2e38fdf2184a2680613d9b46d2aeef205
Alind Sharma <alindsharmasimply@gmail.com>
2017-11-07 21:11:23 +0530

    More on strings with rising levels

---
+++ nintyNinth.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string s = "hackerrank";
+    int n, k;
+    string x;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        k = 0;
+        for (int j = 0; j < x.length(); j++) {
+            if (s[k] == x[j]) {
+                k++;
+            }
+    }
+    if (k == 10) {
+        cout << "YES" << '\n';
+    }
+    else
+        std::cout << "NO" << '\n';
+}
+    return 0;
+}
